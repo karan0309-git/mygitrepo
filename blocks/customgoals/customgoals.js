2ecaaf2c531d4335463db596fec55484afc64d13
karan0309-git <karangithub0309@gmail.com>
2026-03-12 17:06:35 +0530

    Update customgoals.js

--- blocks/customgoals/customgoals.js
+++ blocks/customgoals/customgoals.js
@@ -1,1 +1,13 @@
-
+export default function decorate(block) {
+  const wrapper = block.children[0];
+  if (!wrapper) return;
+  wrapper.classList.add('goals-head');
+  const firstDiv = wrapper.children[0];
+  const secondDiv = wrapper.children[1];
+  if (firstDiv) {
+    firstDiv.classList.add('goals-title');
+  }
+  if (secondDiv) {
+    secondDiv.classList.add('goals-description');
+  }
+}
