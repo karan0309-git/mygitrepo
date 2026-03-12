65f23f25361ba5ffc1418c4f04f44020aa3fbd46
karan0309-git <karangithub0309@gmail.com>
2026-03-12 15:25:34 +0530

    Update expert.js

--- blocks/expertblock/expert.js
+++ blocks/expertblock/expert.js
@@ -1,7 +1,1 @@
-import { createOptimizedPicture } from '../../scripts/aem.js';
-document.addEventListener('click', (e) => {
-  const btn = e.target.closest('.experts-backtotop');
-  if (!btn) return;
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
 
